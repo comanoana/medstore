36c2a00e9420c84f56b8a03b3ed66392787e1959
comanoana <oanamariarodila95@yahoo.es>
2021-01-27 21:03:34 +0200

    Add filter on drugs list

--- functions.js
+++ functions.js
@@ -1,28 +1,3 @@
-
-
-
-function insertDrugs(drugs){
-    const tbody= document.querySelector('#table-list tbody');
-    tbody.innerHTML= getDrugsHTML(drugs);
-}
-
-function getDrugsHTML(drugs){
-    return drugs.map(getDrugHTML).join("");
-}
-
-
-function getDrugHTML(drug){
-    return `<tr>
-    <td>${drug.drugName}</td>
-    <td>${drug.category}</td>
-    <td>${drug.expirationDay}</td>
-    <td><a target="_blanck" href="${drug.link}">Prospectus</a></td>
-    <td>${drug.quantity}</td>
-    <td>
-        <a href="#" class="delete-row">&#10006;</a>
-        <a href="#" class="edit-row">&#9998;</a>
-    </td>`
-}
 let allDrugs = []; 
 
 function loadList(){
@@ -31,6 +6,57 @@ function loadList(){
     .then (data => {
     allDrugs = data;
     insertDrugs(data);
-});
+    });
 };
+
+function insertDrugs(drugs) {
+    const tbody = document.querySelector('#table-list tbody');
+    tbody.innerHTML= getDrugsHTML(drugs);
+}
+
+function getDrugsHTML(drugs){
+    return drugs.map(getDrugHTML).join("");
+}
+
+function getDrugHTML(drug){
+        return `<tr>
+        <td>${drug.drugName}</td>
+        <td>${drug.category}</td>
+        <td>${drug.expirationDay}</td>
+        <td><a target="_blanck" href="${drug.link}">Prospectus</a></td>
+        <td>${drug.quantity}</td>
+        <td>
+            <a href="#" class="delete-row">&#10006;</a>
+            <a href="#" class="edit-row">&#9998;</a>
+        </td>`
+}
+
+function getExpiredDrugHTML(){
+    let expiredDrugs = allDrugs.filter(drug => drug.isExpired == true)
+    insertDrugs(expiredDrugs)
+}
+
+function getUnexpiredDrugHTML() {
+    let unexpiredDrugs = allDrugs.filter(drug => drug.isExpired == false)
+    insertDrugs(unexpiredDrugs)
+}
+
+function addEventListeners() {
+    const expiredBtn = document.getElementById("expiredBtn")
+    expiredBtn.addEventListener("click", (e) => {
+        getExpiredDrugHTML()   
+    })
+
+    const unexpiredBtn = document.getElementById("unexpiredBtn")
+    unexpiredBtn.addEventListener("click", (e) => {
+        getUnexpiredDrugHTML()   
+    })
+
+    const allBtn = document.getElementById("allBtn")
+    allBtn.addEventListener("click", (e) => {
+        insertDrugs(allDrugs)   
+    })
+} 
+
 loadList();
+addEventListeners();
