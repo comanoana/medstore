29aac0cba7ee4879270bfc3ed6156b459dec9435
comanoana <oanamariarodila95@yahoo.es>
2021-01-27 19:28:53 +0200

    Add store.json file as a source file.

--- functions.js
+++ functions.js
@@ -1,1 +1,36 @@
- function insertdrugs(){}
+
+
+
+function insertDrugs(drugs){
+    const tbody= document.querySelector('#table-list tbody');
+    tbody.innerHTML= getDrugsHTML(drugs);
+}
+
+function getDrugsHTML(drugs){
+    return drugs.map(getDrugHTML).join("");
+}
+
+
+function getDrugHTML(drug){
+    return `<tr>
+    <td>${drug.drugName}</td>
+    <td>${drug.category}</td>
+    <td>${drug.expirationDay}</td>
+    <td><a target="_blanck" href="${drug.link}">Prospectus</a></td>
+    <td>${drug.quantity}</td>
+    <td>
+        <a href="#" class="delete-row">&#10006;</a>
+        <a href="#" class="edit-row">&#9998;</a>
+    </td>`
+}
+let allDrugs = []; 
+
+function loadList(){
+    fetch("store.json")
+    .then(res => res.json())
+    .then (data => {
+    allDrugs = data;
+    insertDrugs(data);
+});
+};
+loadList();
